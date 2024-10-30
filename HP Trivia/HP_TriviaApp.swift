6f2c5305c72ed993972e9efefec92a275f96d93e
Mark Santoro <mark_santoro@comcast.net>
2024-10-30 11:21:58 -0700

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Mark Santoro on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
