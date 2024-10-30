4572ea834177c0161749a256a023046a1d056b13
Mark Santoro <mark_santoro@comcast.net>
2024-10-30 11:53:02 -0700

    start content view updates

---
+++ HP Trivia/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by Mark Santoro on 10/30/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+    
+}
--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -9,14 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader {geo in
+            ZStack{
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                
+                VStack{
+                    VStack{
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        
+                        Text("HP")
+                            .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50)
+                        
+                        Text("Trivia")
+                            .font(.custom(Constants.hpFont, size: 60))
+                    }
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+                
+                
+                
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+            
+            
         }
-        .padding()
+        .ignoresSafeArea()
     }
+    
 }
 
 #Preview {
