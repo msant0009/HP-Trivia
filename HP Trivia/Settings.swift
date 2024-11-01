2ffc454cbea2c9f5693495a37270e86d07287010
Mark Santoro <mark_santoro@comcast.net>
2024-11-01 13:18:10 -0700

    start of settings view

---
+++ HP Trivia/Settings.swift
@@ -0,0 +1,72 @@
+//
+//  Settings.swift
+//  HP Trivia
+//
+//  Created by Mark Santoro on 11/1/24.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        ZStack{
+            InfoBackgroundImage()
+            
+            VStack{
+             Text("Which books would you like to see questions from?")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(), GridItem()]) {
+                        ZStack(alignment: .bottomTrailing){
+                            Image("hp1")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                            
+                            Image(systemName:
+                                    "checkmark.circle.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                            .foregroundColor(.green)
+                            .shadow(radius: 1)
+                            .padding(3)
+                            
+                        }
+                        
+                        ZStack(alignment: .bottomTrailing){
+                            Image("hp2")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                                .overlay(Rectangle().opacity(0.33))
+                            
+                            Image(systemName: "circle")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .foregroundColor(.green.opacity(0.5))
+                                .shadow(radius: 1)
+                                .padding(3)
+                        }
+                    }
+                        .padding()
+                    }
+                
+                
+                Button("Done") {
+                    dismiss()
+                }
+                
+                
+                .doneButton() // see constants file
+            }
+        }
+    }
+}
+
+#Preview {
+    Settings()
+}
