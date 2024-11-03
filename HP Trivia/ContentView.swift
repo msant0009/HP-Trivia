7b3658e8b03cbe7162444ad42e48d8baf28c7ac0
Mark Santoro <mark_santoro@comcast.net>
2024-11-03 09:38:11 -0800

    create game play view

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
     @State private var animateViewsIn = false
     @State private var showInstructions = false
     @State private var showSettings = false
+    @State private var playGame = false
+    
+    
     
     var body: some View {
         GeometryReader {geo in
@@ -107,8 +110,7 @@ struct ContentView: View {
                             if animateViewsIn {
                                 
                                 Button {
-                                    
-                                    // start new game
+                                    playGame.toggle()
                                     
                                 } label: {
                                     Text("Play")
@@ -128,6 +130,9 @@ struct ContentView: View {
                                     }
                                 }
                                 .transition(.offset(y: geo.size.height/3))
+                                .fullScreenCover(isPresented: $playGame) {
+                                    GamePlay()
+                                }
                             }
                         }
                         .animation(.easeOut(duration: 0.7).delay(2),value:animateViewsIn)
---
+++ HP Trivia/GamePlay.swift
@@ -0,0 +1,105 @@
+//
+//  GamePlay.swift
+//  HP Trivia
+//
+//  Created by Mark Santoro on 11/3/24.
+//
+
+import SwiftUI
+
+
+struct GamePlay: View {
+   
+    var body: some View {
+        GeometryReader {geo in
+            ZStack{
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width*3, height: geo.size.height*1.05)
+                    .overlay(Rectangle().foregroundColor(.black.opacity(0.8)))
+                
+                VStack {
+                    HStack{
+                        Button("End Game") {
+                            // end the game
+                          
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.red.opacity(0.5))
+                        
+                        Spacer()
+                        
+                        Text("Score: 33")
+                    }
+                    .padding()
+                    .padding(.vertical, 30)
+                    
+                    
+                    Text("Who is Harry Potter?")
+                        .font(.custom(Constants.hpFont, size: 50))
+                    
+                    
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    HStack{
+                        
+ 
+                        Image(systemName: "questionmark.app.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                            .foregroundColor(.cyan)
+                            .rotationEffect(.degrees(-15))
+                            .padding()
+                            .padding(.leading, 20)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "book.closed")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50)
+                            .foregroundColor(.black)
+                            .frame(width: 100, height: 100)
+                            .background(.cyan)
+                            .cornerRadius(20)
+                            .rotationEffect(.degrees(15))
+                            .padding()
+                            .padding(.trailing, 20)
+                        
+                    }
+                    .padding(.bottom)
+                    
+                    LazyVGrid(columns: [GridItem(), GridItem()]) {
+                        ForEach(1..<5) {i in
+                            Text(i==3 ? "The boy who lived and got sent to his relatives house where he was traeted badly kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" : "Answer \(i)")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .padding(10)
+                                .frame(width: geo.size.width/2.15, height: 80)
+                                .background(.green.opacity(0.5))
+                                .cornerRadius(25)
+                            
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+                .foregroundColor(.white)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+            
+            
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    GamePlay()
+}
