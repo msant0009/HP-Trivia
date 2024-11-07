4b4865f20c6731681ab5e639ff72cb6c9ab49bbb
Mark Santoro <mark_santoro@comcast.net>
2024-11-07 10:03:35 -0800

    add functionality to Store file

--- HP Trivia/Store.swift
+++ HP Trivia/Store.swift
@@ -18,7 +18,14 @@ enum BookStatus {
 class Store: ObservableObject {
     @Published var books: [BookStatus] = [.active,.active,.inactive,.locked,.locked,.locked,.locked]
     @Published var products: [Product] = []
+    @Published var purchasedIDs = Set<String>()
+    
     private var productIDs = ["hp4","hp5","hp6","hp7"]
+    private var updates: Task<Void, Never>? = nil
+    
+    init() {
+        updates = watchForUpdates()
+    }
     
     func loadProducts() async {
         do {
@@ -26,6 +33,72 @@ class Store: ObservableObject {
         } catch {
             print("Couldn't fetch those products \(error)")
         }
+    }
+    
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+            case .success(let verificationResult):
+                // purchase successful, but need receipt verification
+                switch verificationResult {
+                case .unverified(let signedType, let verificationError):
+                    print("Error on \(signedType): \(verificationError)")
+                    
+                case .verified(let signedType):
+                    purchasedIDs.insert(signedType.productID)
+                }
+                
+            case .userCancelled:
+                break
+                //User cancelled or purchase dissaproved
+            case .pending:
+                break
+                // waiting for approval
+            @unknown default:
+                break
+            }
+            
+            
+        }catch {
+            print("Couldn't purchase product \(error)")
+        }
+        
+        
+        
+    }
+    
+    private func checkPurchases() async {
+        for product in products {
+            guard let state = await product.currentEntitlement else {return}
+            
+            switch state {
+            case .unverified(let signedType, let verificationError):
+                print("Error on \(signedType): \(verificationError)")
+            case .verified(let signedType):
+                if signedType.revocationDate == nil {
+                    purchasedIDs.insert(signedType.productID)
+                } else {
+                    purchasedIDs.remove(signedType.productID)
+                }
+                
+                
+            }
+            
+            
+        }
+    }
+    
+    // in case the user bought outside of the app (AppStore)
+    private func watchForUpdates() -> Task<Void, Never> {
+        Task(priority: .background) {
+            for await _ in Transaction.updates {
+                await checkPurchases()
+            }
+        }
+        
+        
     }
 }
 
